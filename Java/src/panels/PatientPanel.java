a19652d0326cdd09f69506583a4ec4f002c9e0c7
Gionatha Pirola <g.pirola3@studenti.unibg.it>
2024-08-24 16:15:24 +0200

    add unit measure

--- Java/src/panels/PatientPanel.java
+++ Java/src/panels/PatientPanel.java
@@ -29,6 +29,9 @@ public class PatientPanel {
     private JTextField nameField_Patient, ageField_Patient, weightField_Patient, heightField_Patient, bodyFatField_Patient;
     private JTextField heartRateField_Patient, diastolicField_Patient, systolicField_Patient, respirationRateField_Patient, basalMetabolicRateField_Patient;
     JComboBox<String> sexComboBox_Patient = new JComboBox<>(new String[]{"Male", "Female"});
+    private JComboBox<String> weightUnitComboBox, heightUnitComboBox;
+    
+    
     private JPanel patientPanel = new JPanel();
     private String selectedFilePath;
     
@@ -45,17 +48,36 @@ public class PatientPanel {
         gbc.gridx = 0;
         gbc.gridy = 0;
              
-        addLabelAndField("Name:", nameField_Patient = new JTextField("Standard"), patientPanel, gbc);
+        
+        addLabelAndField("Name:", nameField_Patient = new JTextField("Standard", 20), patientPanel, gbc);
         addLabelAndField("Sex:", sexComboBox_Patient, patientPanel, gbc); 
-        addLabelAndField("Age (years):", ageField_Patient = new JTextField("44"), patientPanel, gbc);
-        addLabelAndField("Weight (lbs):", weightField_Patient = new JTextField("170"), patientPanel, gbc);
-        addLabelAndField("Height (inches):", heightField_Patient = new JTextField("71"), patientPanel, gbc);
-        addLabelAndField("Body Fat Fraction:", bodyFatField_Patient = new JTextField("0.21"), patientPanel, gbc);
-        addLabelAndField("Heart Rate Baseline:", heartRateField_Patient = new JTextField("72"), patientPanel, gbc);
-        addLabelAndField("Diastolic Pressure (mmHg):", diastolicField_Patient = new JTextField("72"), patientPanel, gbc);
-        addLabelAndField("Systolic Pressure (mmHg):", systolicField_Patient = new JTextField("114"), patientPanel, gbc);
-        addLabelAndField("Respiration Rate Baseline:", respirationRateField_Patient = new JTextField("16"), patientPanel, gbc);
-        addLabelAndField("Basal Metabolic Rate (kcal/day):", basalMetabolicRateField_Patient = new JTextField("1600"), patientPanel, gbc);
+        addLabelFieldAndUnit("Age:", ageField_Patient = new JTextField("44"),new JLabel(""), patientPanel, gbc);
+
+        // JComboBox per peso
+        weightUnitComboBox = new JComboBox<>(new String[]{"lbs", "kg"});
+        addLabelFieldAndUnit("Weight:", weightField_Patient = new JTextField("170"), weightUnitComboBox, patientPanel, gbc);
+
+        // JComboBox per altezza
+        heightUnitComboBox = new JComboBox<>(new String[]{"inches", "m", "cm", "ft"});
+        addLabelFieldAndUnit("Height:", heightField_Patient = new JTextField("71"), heightUnitComboBox, patientPanel, gbc);
+
+        // JLabel per body fat fraction
+        addLabelFieldAndUnit("Body Fat Fraction:", bodyFatField_Patient = new JTextField("0.21"), new JLabel(""), patientPanel, gbc);
+
+        // JLabel per heart rate
+        addLabelFieldAndUnit("Heart Rate Baseline:", heartRateField_Patient = new JTextField("72"), new JLabel("mmHg"), patientPanel, gbc);
+
+        // JLabel per diastolic pressure
+        addLabelFieldAndUnit("Diastolic Pressure:", diastolicField_Patient = new JTextField("72"), new JLabel("mmHg"), patientPanel, gbc);
+        
+        // JLabel per systolic pressure
+        addLabelFieldAndUnit("Systolic Pressure:", systolicField_Patient = new JTextField("114"), new JLabel("mmHg"), patientPanel, gbc);
+
+        // JLabel per respiration rate
+        addLabelFieldAndUnit("Respiration Rate Baseline:", respirationRateField_Patient = new JTextField("16"), new JLabel("breaths/min"), patientPanel, gbc);
+
+        // JLabel per basal metabolic rate
+        addLabelFieldAndUnit("Basal Metabolic Rate:", basalMetabolicRateField_Patient = new JTextField("1600"), new JLabel("kcal/day"), patientPanel, gbc);
 
         gbc.gridx = 0;
         gbc.gridy++;
@@ -128,8 +150,10 @@ public class PatientPanel {
                     if(sex.isBlank()) sex = "Male";
                     int age = rootNode.path("CurrentPatient").path("Age").path("ScalarTime").path("Value").asInt();
                     double weight = rootNode.path("CurrentPatient").path("Weight").path("ScalarMass").path("Value").asDouble();
+                    String weightUnit = rootNode.path("CurrentPatient").path("Weight").path("ScalarMass").path("Unit").asText();
                     int height = rootNode.path("CurrentPatient").path("Height").path("ScalarLength").path("Value").asInt();
-                    double bodyFat = rootNode.path("CurrentPatient").path("BodyFatFraction").path("Scalar0To1").path("Value").asDouble();
+                    String heightUnit = rootNode.path("CurrentPatient").path("Height").path("ScalarLength").path("Unit").asText();
+                     double bodyFat = rootNode.path("CurrentPatient").path("BodyFatFraction").path("Scalar0To1").path("Value").asDouble();
                     double heartRate = rootNode.path("CurrentPatient").path("HeartRateBaseline").path("ScalarFrequency").path("Value").asDouble();
                     double diastolicPressure = rootNode.path("CurrentPatient").path("DiastolicArterialPressureBaseline").path("ScalarPressure").path("Value").asDouble();
                     double systolicPressure = rootNode.path("CurrentPatient").path("SystolicArterialPressureBaseline").path("ScalarPressure").path("Value").asDouble();
@@ -141,13 +165,19 @@ public class PatientPanel {
                     sexComboBox_Patient.setSelectedItem(sex);
                     ageField_Patient.setText(String.valueOf(age));
                     weightField_Patient.setText(String.format("%.2f", weight));
+                    weightUnitComboBox.setSelectedItem(convertWeightUnitToComboBoxValue(weightUnit));
                     heightField_Patient.setText(String.valueOf(height));
+                    heightUnitComboBox.setSelectedItem(convertHeightUnitToComboBoxValue(heightUnit));
                     bodyFatField_Patient.setText(String.format("%.2f", bodyFat));
                     heartRateField_Patient.setText(String.format("%.2f", heartRate));
                     diastolicField_Patient.setText(String.format("%.2f", diastolicPressure));
                     systolicField_Patient.setText(String.format("%.2f", systolicPressure));
                     respirationRateField_Patient.setText(String.valueOf(respirationRate));
                     basalMetabolicRateField_Patient.setText(String.format("%.2f", basalMetabolicRate));
+                    
+                    
+                    
+
                     startButton.doClick();
                     startFromFileButton.setEnabled(false);
                 } catch (IOException ex) {
@@ -159,18 +189,61 @@ public class PatientPanel {
         
     }
     
-    public JPanel getPatientPanel() {
-    	return patientPanel;
+
+    private String convertWeightUnitToComboBoxValue(String unit) {
+        switch (unit) {
+            case "lb":
+                return "lbs";
+            case "kg":
+                return "kg";
+            default:
+                return "lbs"; // Valore di default
+        }
+    }
+
+    private String convertHeightUnitToComboBoxValue(String unit) {
+        switch (unit) {
+            case "in":
+                return "inches";
+            case "m":
+                return "m";
+            case "cm":
+                return "cm";
+            case "ft":
+                return "ft";
+            default:
+                return "inches"; // Valore di default
+        }
     }
     
     private void addLabelAndField(String labelText, JComponent component, JPanel panel, GridBagConstraints gbc) {
         gbc.gridx = 0;
         gbc.gridwidth = 1;
+        gbc.weightx = 0; // Il label non espande lo spazio
+        panel.add(new JLabel(labelText), gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER; // Il campo di testo si espande orizzontalmente
+        gbc.weightx = 1.0; // Il campo di testo espande per occupare lo spazio disponibile
+        panel.add(component, gbc);
+        
+        gbc.gridy++;
+    }
+
+    
+    
+    
+    private void addLabelFieldAndUnit(String labelText, JComponent component, JComponent unitComponent, JPanel panel, GridBagConstraints gbc) {
+        gbc.gridx = 0;
+        gbc.gridwidth = 1;
         panel.add(new JLabel(labelText), gbc);
         gbc.gridx = 1;
         panel.add(component, gbc);
+        gbc.gridx = 2;
+        panel.add(unitComponent, gbc);
         gbc.gridy++;
     }
+
     
     private void setFieldsEnabled(boolean enabled) {
         nameField_Patient.setEnabled(enabled);
@@ -186,12 +259,16 @@ public class PatientPanel {
         sexComboBox_Patient.setEnabled(enabled);
     }
     
+    public JPanel getPatientPanel() {
+    	return patientPanel;
+    }
+    
     //Get del file (se selezionato)
     public String getSelectedFilePath() {
         return selectedFilePath;
     }
     
-  //Get dati paziente
+    //Get dati paziente
     public String getName_PATIENT() {
         return nameField_Patient.getText();
     }
@@ -234,5 +311,13 @@ public class PatientPanel {
 
     public String getBasalMetabolicRate_PATIENT() {
         return basalMetabolicRateField_Patient.getText();
+    }
+    
+    public String getWeightUnit_PATIENT() {
+    	return (String) weightUnitComboBox.getSelectedItem();
+    }
+    
+    public String getHeightUnit_PATIENT() {
+    	return (String) heightUnitComboBox.getSelectedItem();
     }
 }
