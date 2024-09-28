865a1ceaddda04c155a0000c47819211ebf38e03
Alessandro Colombo <doubleale001@gmail.com>
2024-09-28 12:03:51 +0200

    Bug fixes

--- Java/src/panels/ChartsPanel.java
+++ Java/src/panels/ChartsPanel.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
