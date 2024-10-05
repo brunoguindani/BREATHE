ec9688d65da60b448a59be09c78438a1c9755512
Alessandro Colombo <doubleale001@gmail.com>
2024-10-05 18:24:21 +0200

    Patient, Control, Log and App panels for GUI

--- breathe.gui/src/app/App_temp.java
+++ breathe.gui/src/app/App_temp.java
@@ -1,6 +1,13 @@
 package app;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
+
+import panels.*;
 
 public class App_temp extends JFrame {
 	
@@ -9,7 +16,97 @@ public class App_temp extends JFrame {
 	 */
     private static final long serialVersionUID = 1L;
     
+    //All panels
+    public ControlPanel controlPanel = new ControlPanel(this);
+    public PatientPanel patientPanel = new PatientPanel(this);
+    public ConditionsPanel conditionsPanel = new ConditionsPanel(this);
+    public ActionsPanel actionsPanel = new ActionsPanel(this);
+    public VentilatorsPanel ventilatorsPanel = new VentilatorsPanel(this);
+    public OutputPanel outputPanel = new OutputPanel(this);
+    public LogPanel logPanel = new LogPanel(this);
+    public ScenarioPanel scenarioPanel = new ScenarioPanel(this);
+    public MinilogPanel minilogPanel = new MinilogPanel(this);
+    
+    //Left and Right Panel
+    public JTabbedPane leftTabbedPane;
+    public JTabbedPane rightTabbedPane; 
+    
+    //Panel for switching between patient and condition during setUp
+    private JPanel patientConditionPanel;
+    private CardLayout cardLayout;
+    
     public App_temp() {
     	
+    	//Main Panel Style
+        setTitle("Breathe Simulation");
+        setSize(1250, 700);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(new BorderLayout());
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+
+        /*
+         * Set up switch between Patient and Condition
+         */
+        JToggleButton patientRadioButton = new JToggleButton("Patient");
+        JToggleButton conditionsRadioButton = new JToggleButton("Conditions");
+        patientRadioButton.setPreferredSize(new Dimension(150, 30));
+        conditionsRadioButton.setPreferredSize(new Dimension(150, 30));
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(patientRadioButton);
+        group.add(conditionsRadioButton);
+        patientRadioButton.setSelected(true);  
+        
+        JPanel radioPanel = new JPanel();
+        radioPanel.setBackground(Color.LIGHT_GRAY); 
+        radioPanel.add(patientRadioButton);
+        radioPanel.add(conditionsRadioButton);
+
+        cardLayout = new CardLayout();
+        patientConditionPanel = new JPanel(cardLayout);
+        patientConditionPanel.add(patientPanel.getMainPanel(), "Patient");
+        patientConditionPanel.add(conditionsPanel.getMainPanel(), "Condition");
+
+        patientRadioButton.addActionListener(e -> cardLayout.show(patientConditionPanel, "Patient"));
+        conditionsRadioButton.addActionListener(e -> cardLayout.show(patientConditionPanel, "Condition"));
+
+        JPanel switchPanel = new JPanel(new BorderLayout());
+        switchPanel.add(radioPanel, BorderLayout.NORTH);
+        switchPanel.add(patientConditionPanel, BorderLayout.CENTER);
+
+        /*
+         * Left side of the main panel
+         */
+        leftTabbedPane = new JTabbedPane();
+        leftTabbedPane.addTab("Patient", switchPanel);
+        leftTabbedPane.addTab("Actions", actionsPanel.getMainPanel());
+        leftTabbedPane.addTab("Ventilators", ventilatorsPanel.getMainPanel());
+        leftTabbedPane.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.DARK_GRAY));
+        
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.add(leftTabbedPane, BorderLayout.CENTER);        
+        
+        /*
+         * Right side of the main panel
+         */
+        rightTabbedPane = new JTabbedPane();
+        rightTabbedPane.addTab("Output", outputPanel.getMainPanel());
+        rightTabbedPane.addTab("Scenario", scenarioPanel.getMainPanel());
+        rightTabbedPane.addTab("Log", logPanel.getMainPanel());
+        rightTabbedPane.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.DARK_GRAY));
+        
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.add(rightTabbedPane, BorderLayout.CENTER);
+
+        /*
+         * Splitting View
+         */
+        add(controlPanel.getMainPanel(), BorderLayout.WEST);  
+        add(leftPanel, BorderLayout.CENTER);  
+        add(rightPanel, BorderLayout.EAST); 
+        add(minilogPanel.getMainPanel(), BorderLayout.SOUTH);
     }
 }
---
+++ breathe.gui/src/panels/PatientPanel.java
@@ -0,0 +1,134 @@
+package panels;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import app.App_temp;
+
+public class PatientPanel {
+	
+
+	/*
+	 * Fields
+	 */
+	private Map<String, JTextField> fieldMap = new HashMap<>();
+    private JComboBox<String> sexComboBox_Patient = new JComboBox<>(new String[]{"Male", "Female"});
+    private JComboBox<String> weightUnitComboBox, heightUnitComboBox;
+	
+	/*
+	 * Inner Panels
+	 */
+	private JScrollPane dataPanel;
+    private JPanel mainPanel = new JPanel();
+    
+    public PatientPanel(App_temp app) {
+    	
+    	//set up main panel
+    	mainPanel.setBackground(Color.LIGHT_GRAY);
+    	mainPanel.setPreferredSize(new Dimension(550, 700));
+    	
+    	//set up dataPanel
+    	dataPanel = new JScrollPane();
+        JPanel innerPanel = new JPanel(); 
+        innerPanel.setLayout(new GridBagLayout());
+        innerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        innerPanel.setBackground(Color.LIGHT_GRAY);
+        
+        //Grid
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        
+        //fill up map
+        fieldMap.put("Name", new JTextField("Standard", 20));
+        fieldMap.put("Age", new JTextField("44"));
+        fieldMap.put("Weight", new JTextField("77"));
+        fieldMap.put("Height", new JTextField("180"));
+        fieldMap.put("BodyFatFraction", new JTextField("0.21"));
+        fieldMap.put("HeartRateBaseline", new JTextField("72"));
+        fieldMap.put("DiastolicArterialPressureBaseline", new JTextField("72"));
+        fieldMap.put("SystolicArterialPressureBaseline", new JTextField("114"));
+        fieldMap.put("RespirationRateBaseline", new JTextField("16"));
+        fieldMap.put("BasalMetabolicRate", new JTextField("1600"));
+        
+        // Selectors for patient data
+        addLabelAndField("Name:", fieldMap.get("Name"), innerPanel, gbc);
+        addLabelAndField("Sex:", sexComboBox_Patient, innerPanel, gbc);
+        addLabelFieldAndUnit("Age:", fieldMap.get("Age"), new JLabel("yr"), innerPanel, gbc);
+        weightUnitComboBox = new JComboBox<>(new String[]{"kg", "lbs"});
+        addLabelFieldAndUnit("Weight:", fieldMap.get("Weight"), weightUnitComboBox, innerPanel, gbc);
+        heightUnitComboBox = new JComboBox<>(new String[]{"cm", "m", "inches", "ft"});
+        addLabelFieldAndUnit("Height:", fieldMap.get("Height"), heightUnitComboBox, innerPanel, gbc);
+        addLabelFieldAndUnit("Body Fat Fraction:", fieldMap.get("BodyFatFraction"), new JLabel("%"), innerPanel, gbc);
+        addLabelFieldAndUnit("Heart Rate Baseline:", fieldMap.get("HeartRateBaseline"), new JLabel("heartbeats/min"), innerPanel, gbc);
+        addLabelFieldAndUnit("Diastolic Pressure:", fieldMap.get("DiastolicArterialPressureBaseline"), new JLabel("mmHg"), innerPanel, gbc);
+        addLabelFieldAndUnit("Systolic Pressure:", fieldMap.get("SystolicArterialPressureBaseline"), new JLabel("mmHg"), innerPanel, gbc);
+        addLabelFieldAndUnit("Respiration Rate Baseline:", fieldMap.get("RespirationRateBaseline"), new JLabel("breaths/min"), innerPanel, gbc);
+        addLabelFieldAndUnit("Basal Metabolic Rate:", fieldMap.get("BasalMetabolicRate"), new JLabel("kcal/day"), innerPanel, gbc);
+        
+        //Add labels
+        fieldMap.get("Age").setToolTipText("Value must be between 18 and 65");
+        fieldMap.get("Height").setToolTipText("Value must be between 163cm and 190cm for male patients and between 151cm and 175cm for female patients");
+        fieldMap.get("BodyFatFraction").setToolTipText("Value must be between 0.02% and 0.25% for male patients and between 0.1% and 0.32% for female patients");
+        fieldMap.get("HeartRateBaseline").setToolTipText("Value must be between 50bpm and 110bpm");
+        fieldMap.get("DiastolicArterialPressureBaseline").setToolTipText("Value must be between 60mmHg and 80mmHg");
+        fieldMap.get("SystolicArterialPressureBaseline").setToolTipText("Value must be between 90mmHg and 120mmHg");
+        fieldMap.get("RespirationRateBaseline").setToolTipText("Value must be between 8bpm and 20bpm");
+        
+        //Add the new panel to the scrollable View
+        dataPanel.setViewportView(innerPanel);
+        dataPanel.setPreferredSize(new Dimension(500, 450)); 
+        dataPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        
+        //Include everything to the main Panel
+        mainPanel.add(dataPanel, BorderLayout.CENTER);
+    }
+    
+    //method to return panel
+    public JPanel getMainPanel() {
+    	return mainPanel;
+    }
+    
+    //method to add visual to panel
+    private void addLabelAndField(String labelText, JComponent component, JPanel innerPanel, GridBagConstraints gbc) {
+        gbc.gridx = 0;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0; 
+        innerPanel.add(new JLabel(labelText), gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER; 
+        gbc.weightx = 1.0; 
+        innerPanel.add(component, gbc);
+        
+        gbc.gridy++;
+    }
+  
+    //method to add visual to panel
+    private void addLabelFieldAndUnit(String labelText, JComponent component, JComponent unitComponent, JPanel innerPanel, GridBagConstraints gbc) {
+        gbc.gridx = 0;
+        gbc.gridwidth = 1;
+        innerPanel.add(new JLabel(labelText), gbc);
+        gbc.gridx = 1;
+        innerPanel.add(component, gbc);
+        gbc.gridx = 2;
+        innerPanel.add(unitComponent, gbc);
+        gbc.gridy++;
+    }
+}
