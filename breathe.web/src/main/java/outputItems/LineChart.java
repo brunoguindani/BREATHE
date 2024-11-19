36d4ff8d3272479fd47507bfbc303fa548f8e1a1
Gionatha Pirola <g.pirola3@studenti.unibg.it>
2024-11-19 12:19:48 +0100

    test charts

--- breathe.web/src/main/java/outputItems/LineChart.java
+++ breathe.web/src/main/java/outputItems/LineChart.java
@@ -1,27 +1,35 @@
 package outputItems;
 
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.page.Push;
 
 @Route
 @Push
 public class LineChart extends ItemDisplay {
-	/*
-	 * Item connected to javascript to display dynamic line charts
-	 */
     private static final long serialVersionUID = 1L;
 
+    private final Chart chart; 
+    private final ListSeries series; 
 
     public LineChart(String title, String unit) {
         super(title, unit);
-        initializeChart(title); 
-    }
 
-    private void initializeChart(String title) {
+        chart = new Chart(ChartType.LINE);
+        chart.setWidthFull();
+        chart.setHeight("300px");
+        chart.getConfiguration().getChart().setStyledMode(true);
+        
+        series = new ListSeries();
+        chart.getConfiguration().addSeries(series);
+
+        add(chart);
     }
 
     @Override
     public void addPoint(double x, double y) {
+        series.addData(y);
     }
 
     @Override
